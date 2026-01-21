e997d3c039067dae564f1876c992b36a16b3d6f4
GayatriPatil2005 <gmpatil9913@gmail.com>
2026-01-21 11:02:40 +0530

    First react demo web

---
+++ Demo/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+     <div id="navbar" >
+      <p id="logo">Webfolio X</p>
+      <ul id='components'>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>Pages</li>
+      </ul>
+     </div>
+     <div id='middle'>
+      <h1>Webfolio X Webflow Template and UI Kit</h1>
+      <h6>Presenting Webfolio X, our ultimate multi purpose portfolio Webflow Template & UI Kit.</h6>
+      <div id='btns'>
+        <p>Get template</p>
+        <p>Browse pages</p>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
